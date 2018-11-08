2b1e676f98a6d13dcf23b9bea07a50afa09fbafb
Adnan Omanovic <adnan.omanovic@serapion-it.com>
2018-11-08 15:19:48 +0100

    Spring boot generated app

---
+++ todos-embedded-mongodb/src/main/java/com/example/todosembeddedmongodb/TodosEmbeddedMongodbApplication.java
@@ -0,0 +1,12 @@
+package com.example.todosembeddedmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodosEmbeddedMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodosEmbeddedMongodbApplication.class, args);
+	}
+}
